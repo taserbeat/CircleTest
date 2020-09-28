d5781c6b7b984f219bae471f660e094a68924772
taserbeat <taserbeat@gmail.com>
2020-09-28 21:48:41 +0900

    switch processing by status code

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ import requests
 URL = 'https://api.github.com/zen'
 response = requests.get(URL)
 
-print(response.status_code)
-print(response.text)
+if response.status_code is 200:
+    print(response.text)
+else:
+    print("Error: ", response.status_code)
